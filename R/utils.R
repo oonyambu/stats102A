12e13edcf7dcedc8f5c2016616337e7fefd10a5a
onyambu onyambu <37595452+oonyambu@users.noreply.github.com>
2020-07-02 19:40:43 -0700

    Update utils.R
    stop if student function doesnt exist

--- R/utils.R
+++ R/utils.R
@@ -9,76 +9,91 @@ teacher$opts_stats102A <- list(
 
 teacher$opts_stats102A_use <- teacher$opts_stats102A
 
-comp <- function(x, studentFUN, correctFUN, class_value = "numeric") {
-  student <- try(
-    {
+comp <-
+  function(x, studentFUN, correctFUN, class_value = "numeric") {
+    stopifnot(is.function(studentFUN), is.function(correctFUN))
+    student <- try({
       setTimeLimit(teacher$opts_stats102A_use$time_limit_compute)
       do.call(studentFUN, as.list(x))
     },
-    silent = TRUE
-  )
-  correct <- try(do.call(correctFUN, as.list(x)), silent = TRUE)
-  if (inherits(correct, "try-error"))
-    inherits(student, "try-error") | is.null(student)
-  else {
-    if (inherits(student, "try-error")) {
-      return(remark = simpleError(student)$message)
+    silent = TRUE)
+    correct <- try(do.call(correctFUN, as.list(x)), silent = TRUE)
+    if (inherits(correct, "try-error"))
+      inherits(student, "try-error") | is.null(student)
+    else {
+      if (inherits(student, "try-error")) {
+        return(remark = simpleError(student)$message)
+      }
+      if (length(correct) == 0) {
+        return(length(student) == 0)
+      }
+      a <- all.equal(c(unname(student)), c(unname(correct)))
+      if (is.logical(a))
+        a
+      else
+        FALSE
     }
-    if(length(correct) == 0){
-      return(length(student) == 0)
-    }
-    a <- all.equal(c(unname(student)), c(unname(correct)))
-    if (is.logical(a)) a else FALSE
   }
-}
-teacher$sp <- sprintf("%%%ds", -teacher$opts_stats102A_use$max_space)
+teacher$sp <-
+  sprintf("%%%ds",-teacher$opts_stats102A_use$max_space)
 
 fun_comp <- function(fun_name, stud_env) {
   cat("\t\t", sprintf(teacher$sp, fun_name))
   test_data <- teacher$test_data[[fun_name]]
-  fn <- match(teacher$fun_dict[[fun_name]], nm_s <- names(stud_env), 0)
-  s_f_name <- if (length(nm_s[fn]) == 1) nm_s[fn] else fun_name
+  fn <-
+    match(teacher$fun_dict[[fun_name]], nm_s <- names(stud_env), 0)
+  s_f_name <- if (length(nm_s[fn]) == 1)
+    nm_s[fn]
+  else
+    fun_name
   cat("Done!\n")
   if (!exists(s_f_name, stud_env, inherits = FALSE, mode = "function")) {
     return(c(
       grade = 0,
-      remark = paste(
-        s_f_name,
-        "0. You are missing the",
-        s_f_name, "function"
-      )
+      remark = paste(s_f_name,
+                     "0. You are missing the",
+                     s_f_name, "function")
     ))
   }
-
+  
   stud_fun <- get0(s_f_name, stud_env, "function", FALSE)
   teach_fun <- get0(fun_name, teacher, "function", FALSE)
   if (length(formalArgs(teach_fun)) != length(test_data[[1]])) {
-    stop("Incorrect number of arguments for the test data as compared to ", fun_name, " args")
+    stop("Incorrect number of arguments for the test data as compared to ",
+         fun_name,
+         " args")
   }
   val <- sapply(test_data, comp, stud_fun, teach_fun)
   ln <- as.logical(val)
-  fin_val <- sum(ln, na.rm = TRUE) / length(ln) * teacher$weight[fun_name]
-  if (any(grepl("object.*not found", val))) fin_val <- 0
+  fin_val <-
+    sum(ln, na.rm = TRUE) / length(ln) * teacher$weight[fun_name]
+  if (any(grepl("object.*not found", val)))
+    fin_val <- 0
   remark <- paste(s_f_name, fin_val)
   if (any(is.na(ln) | !ln)) {
     tss <- test_data[is.na(ln) | !ln]
     ln <- lengths(tss)
     tss1 <- tss[ln <= min(ln) + 3]
     if (min(ln) < 4) {
-      not_work <- sapply(
-        sample(tss1, min(c(3, length(tss1)))),
-        function(x) toString(unlist(x))
-      )
+      not_work <- sapply(sample(tss1, min(c(
+        3, length(tss1)
+      ))),
+      function(x)
+        toString(unlist(x)))
     } else {
       not_work <- tss[[which.min(ln)]]
     }
     remark <- paste(
-      s_f_name, round(fin_val, 2), "Your", s_f_name,
+      s_f_name,
+      round(fin_val, 2),
+      "Your",
+      s_f_name,
       "could not work on some data like",
-      paste0(not_work, collapse = "; and "), toString(unique(val[is.na(ln)]))
+      paste0(not_work, collapse = "; and "),
+      toString(unique(val[is.na(ln)]))
     )
   }
-
+  
   c(grade = fin_val, remark = remark)
 }
 
@@ -87,55 +102,55 @@ compare <- function(student_file) {
   studentID <- sub(".*/", "", dirname(student_file))
   stud_env$ID <- studentID
   cat("Grading", studentID, "\n")
-  if(has_install(student_file))
-    return(cbind(ID = studentID,remark = "Installing a package-cannot grade"))
+  if (has_install(student_file))
+    return(cbind(ID = studentID, remark = "Installing a package-cannot grade"))
   scr <- try(source(student_file, stud_env), TRUE)
   if (inherits(scr, "try-error")) {
     return(c(
-      ID = studentID, grade = 0,
+      ID = studentID,
+      grade = 0,
       remark = "Your file could not be sourced!"
     ))
   }
-  s <- cbind(
-    ID = studentID,
-    t(sapply(names(teacher$fun_dict), fun_comp, stud_env))
-  )
+  s <- cbind(ID = studentID,
+             t(sapply(
+               names(teacher$fun_dict), fun_comp, stud_env
+             )))
   cat("\t\t\t\t\tDone with", studentID, "!!!\n\n")
   s
 }
 
 
-make_teacher <- function(student_dir, teacher_file,
-                         function_test_data, weight = 1,
+make_teacher <- function(student_dir,
+                         teacher_file,
+                         function_test_data,
+                         weight = 1,
                          keep_par_names = FALSE,
                          fun_dict = NULL) {
   source(teacher_file, teacher)
   functions_to_test <- names(function_test_data)
   teacher$weight <- set_name(functions_to_test, weight)
   teacher$test_data <- function_test_data
-  teacher$fun_dict <- `names<-`(
-    as.list(functions_to_test),
-    functions_to_test
-  )
+  teacher$fun_dict <- `names<-`(as.list(functions_to_test),
+                                functions_to_test)
   if (!is.null(fun_dict)) {
     teacher$fun_dict <- unstack(stack(c(teacher$fun_dict, fun_dict)))
   }
-
+  
   teacher$keep_par_names <- keep_par_names
 }
 
-has_install <- function(path){
-  any(grepl("^[^#]*install",readLines(path)))
+has_install <- function(path) {
+  any(grepl("^[^#]*install", readLines(path)))
 }
-
-
-
-
 
 
 
 mklst <- function(x, keep_par_names) {
-  if (keep_par_names) as.list(x) else unname(as.list(x))
+  if (keep_par_names)
+    as.list(x)
+  else
+    unname(as.list(x))
 }
 
 
@@ -193,20 +208,27 @@ set_name <- function(funs, weights) {
 knit <- function(path, new_dir, new_file) {
   ID <- sub(".*/", "", dirname(path))
   cat("Knitting", ID, "\n")
-  if(has_install(path)){
-  cat(ID, "Not knittable - Installing a package", "\n",
-      file = new_file, append = TRUE)
-  return(FALSE)
+  if (has_install(path)) {
+    cat(
+      ID,
+      "Not knittable - Installing a package",
+      "\n",
+      file = new_file,
+      append = TRUE
+    )
+    return(FALSE)
   }
-  tried <- try(
-    {
-      setTimeLimit(teacher$opts_stats102A_use$time_limit_knit)
-      rmarkdown::render(path, "html_document",
-        output_dir = new_dir, clean = TRUE, quiet = TRUE
-      )
-    },
-    silent = TRUE
-  )
+  tried <- try({
+    setTimeLimit(teacher$opts_stats102A_use$time_limit_knit)
+    rmarkdown::render(
+      path,
+      "html_document",
+      output_dir = new_dir,
+      clean = TRUE,
+      quiet = TRUE
+    )
+  },
+  silent = TRUE)
   if (inherits(tried, "try-error")) {
     if (grepl("no package called", tried)) {
       mes <- trimws(sub(".*no package called", "", tried))
@@ -214,13 +236,17 @@ knit <- function(path, new_dir, new_file) {
       knit(path, new_dir, new_file)
     }
     else {
-      cat(ID, gsub("\n", "", tried), "\n", file = new_file, append = TRUE)
+      cat(ID,
+          gsub("\n", "", tried),
+          "\n",
+          file = new_file,
+          append = TRUE)
     }
   }
   else {
     cat(ID, "Successful", "\n", file = new_file, append = TRUE)
   }
-  !inherits(tried, "try-error")
+  ! inherits(tried, "try-error")
 }
 
 
@@ -233,10 +259,12 @@ opts <- function(..., reset = FALSE) {
     stop("There are no options for ", toString(nm))
   }
   if (length(y) > 0) {
-    teacher$opts_stats102A_use <- modifyList(teacher$opts_stats102A_use, y)
+    teacher$opts_stats102A_use <-
+      modifyList(teacher$opts_stats102A_use, y)
   }
   if (reset) {
     teacher$opts_stats102A_use <- teacher$opts_stats102A
   }
-  if (!length(y) & !reset) teacher$opts_stats102A_use
+  if (!length(y) & !reset)
+    teacher$opts_stats102A_use
 }

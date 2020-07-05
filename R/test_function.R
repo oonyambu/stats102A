b5ecae633e2bae8a806f72aa0ef8dfb124d130d0
onyambu onyambu <37595452+oonyambu@users.noreply.github.com>
2020-07-05 09:48:45 -0700

    print no_match

--- R/test_function.R
+++ R/test_function.R
@@ -1,8 +1,8 @@
-test_function <- function(test_dat, studentFUN, correctFUN, ...) {
+test_function <- function(test_dat, studentFUN, correctFUN) {
   test_dat <- convt2list(test_dat)
-  a <- sapply(test_dat, comp, studentFUN, correctFUN, ...)
+  a <- sapply(test_dat, comp, studentFUN, correctFUN)
   b <- suppressWarnings(as.logical(a))
-  if (any(is.na(b) | !b)) {
+  if (teacher$no_match & any(is.na(b) | !b)) {
     sprintf(
       "your function differs in %s",
       unlist(toString(test_dat[is.na(b) | !b]))
